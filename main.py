24b30d5091153c3c40a3945324dea3c1314eb5ae
Ben <ben-github@users.noreply.github.com>
2014-11-05 17:32:12 -0700

    Fixed thanks to PyQt mailing list. Basic callback functions with PyQt5 and QML working.

--- main.py
+++ main.py
@@ -3,23 +3,31 @@ import sys
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtQuick import QQuickView
+from PyQt5.QtQml import QJSValue
 
 class iceController(QObject):
     def __init__(self):
         QObject.__init__(self)
-        self.callback = None
+        self.callback = []
+        
 
     def dump(self):
-        print('Callback is %s' % self.callback)
-        print('Callback is callable %s' % self.callback.isCallable)
-        print('Callback is callable %s' % self.callback.isCallable())
-        self.callback.call()
-
+        print('Dump was called')
+        #print('Callback is %s' % self.callback)
+        #print(dir(self.callback))
+        #print('Callback is callable %s' % self.callback.isCallable)
+        #print('Callback is callable %s' % self.callback.isCallable())
+        for c in self.callback:
+            c.call([QJSValue('asdf')])
+        self.callback=[]
     @pyqtSlot(str, 'QJSValue')
     def enqueue(self, command, callback):
         print('Enqueuing function of %s' % command)
-        self.callback = callback
-        self.dump()
+        #print('Test callback is %s' % callback)
+        #print('Callback is callable?:  %s' % callback.isCallable())
+        self.callback.append(QJSValue(callback))
+        #self.callback = callback
+        #self.dump()
 
     @pyqtSlot()
     def processResponses(self):
