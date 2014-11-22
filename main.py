44f434ddf07d36972567dd94b1fa3c178ab64d76
Marcus Ottosson <konstruktion@gmail.com>
2014-11-22 17:52:36 +0000

    - PEP08 compatible - Spaces, not tabs - JSLint compatible - PyFlakses compatible - Grammar - Removing comments - Unifying double- and single-quotes - Use of anonymous functions - Python list identity preserved ([:] = [])

--- main.py
+++ main.py
@@ -5,47 +5,43 @@ from PyQt5.QtWidgets import QApplication
 from PyQt5.QtQuick import QQuickView
 from PyQt5.QtQml import QJSValue
 
-class iceController(QObject):
+import time
+
+
+class IceController(QObject):
     def __init__(self):
         QObject.__init__(self)
         self.callback = []
-        
 
     def dump(self):
-        print('Dump was called')
-        #print('Callback is %s' % self.callback)
-        #print(dir(self.callback))
-        #print('Callback is callable %s' % self.callback.isCallable)
-        #print('Callback is callable %s' % self.callback.isCallable())
+        print("Dump was called")
         for c in self.callback:
-            c.call([QJSValue('asdf')])
-        self.callback=[]
-    @pyqtSlot(str, 'QJSValue')
-    def enqueue(self, command, callback):
-        print('Enqueuing function of %s' % command)
-        #print('Test callback is %s' % callback)
-        #print('Callback is callable?:  %s' % callback.isCallable())
-        self.callback.append(QJSValue(callback))
-        #self.callback = callback
-        #self.dump()
+            time.sleep(1)
+            c.call([QJSValue("asdf")])
+        self.callback[:] = []
 
+    @pyqtSlot(str, "QJSValue")
+    def enqueue(self, command, callback):
+        print("Enqueuing function of %s" % command)
+        self.callback.append(QJSValue(callback))
 
     @pyqtSlot()
     def processResponses(self):
-        print('processing responses')
+        print("processing responses")
         self.dump()
 
     @pyqtSlot(str)
     def log(self, s):
         print(s)
 
-print('Starting ICE Control GUI...')        
+
+print("Starting ICE Control GUI...")
 app = QApplication(sys.argv)
 view = QQuickView()
 context = view.rootContext()
 
-ice = iceController()
-context.setContextProperty('PyConsole', ice)
-context.setContextProperty('ice', ice)
+ice = IceController()
+context.setContextProperty("PyConsole", ice)
+context.setContextProperty("ice", ice)
 
 view.setSource(QUrl("main.qml"))
